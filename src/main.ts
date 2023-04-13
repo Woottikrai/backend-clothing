f33be65ce10d57f20864c42a3581a8710ba504ca
Jay <111434827+Woottikrai@users.noreply.github.com>
2023-04-13 15:40:00 +0700

    setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { urlencoded } from 'express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(urlencoded({ extended: true, limit: '50mb' })); //solution to add image
+  const config = new DocumentBuilder()
+    .setTitle('Clothing-Shop')
+    .setDescription('This is API Clothing-Shop')
+    .setVersion('1.0')
+    .addTag('Clothing-Shop')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  app.useGlobalPipes(new ValidationPipe());
+  SwaggerModule.setup('api', app, document);
+  app.enableCors();
+
+  await app.listen(3001);
 }
 bootstrap();
